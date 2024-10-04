1344318e3e752d4d4d41db5dad40219b9d6a02ca
Chrimau <g.nwokeji@stud.uni-goettingen.de>
2024-10-05 01:06:05 +0200

    added new files to the source repo

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+const name = ref('Susan Patrick')
+const status = ref('active')
+const tasks = ref('task One', 'task two', 'task three')
+</script>
+ []
+
+<template>
+ <h1 class="text-2xl">Vue Jobs</h1>
+ <p>This is an incomplete statement</p>
+</template>
+
---
+++ src/Main.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="text-2xl">Vue Jobs</h1>
+    <p>This is an incomplete statement</p>
+   </template>
